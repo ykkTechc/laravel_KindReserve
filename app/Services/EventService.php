afbba5eb7d78a67b09436e8bf5084366739a158b
ykkTechc <102883569+ykkTechc@users.noreply.github.com>
2023-02-05 23:32:05 +0900

    event store

---
+++ app/Services/EventService.php
@@ -0,0 +1,27 @@
+<?php
+//ファイルが存在する場所
+namespace App\Services;
+
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
+class EventService {
+
+  public  static function checkEventDuplication($eventDate, $startTime, $endTime) {
+    $check = DB::table('events')
+    ->whereDate('start_date', $eventDate) //日にち
+    ->whereTime('end_date', '>', $startTime)
+    ->whereTime('start_date', '<', $endTime)
+    ->exists();
+    return $check;
+  }
+
+  public static function joinDateAndTime($date, $time)
+  {
+    $join = $date. " " . $time;
+    $dateTime = Carbon::createFromFormat('Y-m-d H:i', $join);
+    return $dateTime;
+  }
+
+
+}
--- lang/ja/validation.php
+++ lang/ja/validation.php
@@ -157,6 +157,11 @@ return [
       'name' => '名前',
       'email' => 'メールアドレス',
       'password' => 'パスワード',
+      'event_name' => 'イベント名',
+      'information' => 'イベント詳細',
+      'start_time ' => '開始時間',
+      'end_time' => '終了時間',
+      'max_people' => '定員'
     ],
 
 ];
